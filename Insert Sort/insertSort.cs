4ae0eeb9e49fcefe623a0e62d6d3327251de040a
Manrick-Lazaro <manricksantos@gmail.com>
2025-07-21 18:59:26 -0300

    feat: implements descending sort function in insert sort class

--- Insert Sort/insertSort.cs
+++ Insert Sort/insertSort.cs
@@ -8,16 +8,20 @@ class Program
 
         int[] disorderedElements = new int[] { 5, 4, 2, 3, 1, 42, 99, 54, 76, 78, 35, 64, 5, 4, 23, 6, 76, 45, 24, 32, 77, 54, 56, 6 };
 
-        int[] ascendingElements = insertSortInstance.Ascending(disorderedElements);
+        int[] increasingElements = insertSortInstance.Increasing(disorderedElements);
+        int[] decreasingElements = insertSortInstance.Decreasing(disorderedElements);
 
         Console.WriteLine("Elementos ordenados de forma crescente");
-        Console.WriteLine(string.Join(", ", ascendingElements));
+        Console.WriteLine(string.Join(", ", increasingElements));
+
+        Console.WriteLine("Elementos ordenados de forma decrescente");
+        Console.WriteLine(string.Join(", ", decreasingElements));
     }
 }
 
 class InsertSort
 {
-    public int[] Ascending(int[] elements)
+    public int[] Increasing(int[] elements)
     {
         int[] ordenedElements = (int[])elements.Clone();
 
@@ -27,6 +31,27 @@ class InsertSort
             int i = j - 1;
 
             while (i >= 0 && ordenedElements[i] > key)
+            {
+                ordenedElements[i + 1] = ordenedElements[i];
+                i--;
+            }
+
+            ordenedElements[i + 1] = key;
+        }
+
+        return ordenedElements;
+    }
+
+    public int[] Decreasing(int[] elements)
+    {
+        int[] ordenedElements = (int[])elements.Clone();
+
+        for (int j = 1; j < ordenedElements.Length; j++)
+        {
+            int key = ordenedElements[j];
+            int i = j - 1;
+
+            while (i >= 0 && ordenedElements[i] < key)
             {
                 ordenedElements[i + 1] = ordenedElements[i];
                 i--;
